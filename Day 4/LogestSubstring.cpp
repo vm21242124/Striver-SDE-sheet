6ba39c31caadca297d1cec23c345fdb118831a5f
vm2124 <vm21242124@gmail.com>
2023-06-09 08:10:42 +0530

    longest substring

---
+++ Day 4/LogestSubstring.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h> 
+int uniqueSubstrings(string s)
+{
+    //Write your code here
+        vector<int> dict(256, -1);
+        int maxLen = 0, start = -1;
+        for (int i = 0; i != s.length(); i++) {
+            if (dict[s[i]] > start)
+                start = dict[s[i]];
+            dict[s[i]] = i;
+            maxLen = max(maxLen, i - start);
+        }
+        return maxLen;
+}
